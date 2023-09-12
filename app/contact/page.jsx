278d370910bc022c9efab7e70949cfe9075cc2cb
angelDova <Angelfcb01@yahoo.com>
2023-09-12 15:25:31 -0700

    contact page update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,4 +1,8 @@
+import ContactDetails from "@/components/ContactDetails";
+import ShiftingContactForm from "@/components/ContactForm";
+import Container from "@/components/Container";
 import PageIntro from "@/components/PageIntro";
+import TerminalContact from "@/components/TerminalContact";
 import React from "react";
 
 const ContactPage = () => {
@@ -7,6 +11,13 @@ const ContactPage = () => {
       <PageIntro eyebrow="Contact Me" title="Let's work together">
         <p className="">We can&apos;t wait to hear from you.</p>
       </PageIntro>
+      <Container className="mt-24 sm:mt-32 lg:mt-40">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-24 lg:grid-cols-2">
+          <ContactDetails />
+          {/* <TerminalContact /> */}
+          <ShiftingContactForm />
+        </div>
+      </Container>
     </>
   );
 };
---
+++ components/ContactDetails.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import FadeIn from "./FadeIn";
+import Locations from "./Locations";
+import Border from "./Border";
+import Link from "next/link";
+import SocialMedia from "./SocialMedia";
+
+const ContactDetails = () => {
+  return (
+    <FadeIn>
+      <h2 className="font-display text-base font-semibold text-neutral-950">
+        Location
+      </h2>
+      <p className="mt-6 text-base text-neutral-600">
+        Prefer to do things in person? Reach out to me to set an appointment.
+      </p>
+      <Locations className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2" />
+      <Border className="mt-16 pt-16">
+        <h2 className="font-display text-base font-semibold text-neutral-950">
+          Email us
+        </h2>
+        <dl className="mt-6 grid grid-cols-1 gap-8 text-sm sm:grid-cols-2">
+          {[
+            ["Professional", "dovastudios@gmail.com"],
+            ["Personal", "arianacordova@gmail.com"],
+          ].map(([label, email]) => (
+            <div key={email}>
+              <dt className="font-semibold text-neutral-950">{label}</dt>
+              <dd>
+                <Link
+                  href={`mailto:${email}`}
+                  className="text-neutral-600 hover:text-neutral-950"
+                >
+                  {email}
+                </Link>
+              </dd>
+            </div>
+          ))}
+        </dl>
+      </Border>
+      <Border className="mt-16 pt-16">
+        <h2 className="font-display text-base font-semibold text-neutral-950">
+          Follow Me
+        </h2>
+        <SocialMedia className="mt-6" />
+      </Border>
+    </FadeIn>
+  );
+};
+
+export default ContactDetails;
